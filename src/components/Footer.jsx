13a29529eb35e61863f0543318dcd8f489be9282
isaac844966 <isaackeyz55@gmail.com>
2024-11-19 15:16:37 +0100

    changed download link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import LOGO from "../images/logo.png";
 import Paystack from "../images/Paystack_Logo.svg";
 import visa from "../images/visa.svg";
@@ -16,55 +15,92 @@ import Wrapper from "../assets/wrapper/Footer";
 const Footer = () => {
   return (
     <Wrapper>
-      <div className='footer'>
-        <div className='footer-container'>
-          <div className='footer-content'>
-            <img src={LOGO} alt='logo' title="Logo" loading="lazy" />
-            <p>
-              FollowMeBet is owned and operated by Balary Gaming Network
-              Limited, registered with registration Number: RC - 1946229, which
-              is licensed and regulated by the Oyo state Gaming board to operate
-              activities under the license number: OYSGB/NG/22001
-            </p>
+      <div className="footer">
+        <div className="footer-container">
+          <div className="footer-content">
+            <img src={LOGO} alt="logo" title="Logo" loading="lazy" />
           </div>
-          <div className='footer-links'>
+          <div className="footer-links">
             <h1>Quick links</h1>
-            <div className='links'>
-              <Link to='/'>Home</Link>
-              <Link to='/download'>Download App</Link>
-              <Link to='/contact'>Contact us</Link>
+            <div className="links">
+              <Link to="/">Home</Link>
+              <Link to="/download">Download App</Link>
+              <Link to="/contact">Contact us</Link>
             </div>
           </div>
-          <div className='download-app'>
+          <div className="download-app">
             <h1>Download</h1>
-            <div className='link'>
-              <Link to='/download'>
-                <img src={Android} alt="Android" title="Dowload For Android now" loading="lazy"/>
+            <div className="link">
+              <Link to="/download">
+                <img
+                  src={Android}
+                  alt="Android"
+                  title="Dowload For Android now"
+                  loading="lazy"
+                />
               </Link>
-              <a href="https://apps.apple.com/ng/app/followmebet/id6444085742" target="_blank">
-
-              <img src={ios} alt='ios'title="Download on IOS Now " loading="lazy"  />
+              <a
+                href="https://apps.apple.com/ng/app/followmebet/id6444085742"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  src={ios}
+                  alt="ios"
+                  title="Download on IOS Now "
+                  loading="lazy"
+                />
               </a>
             </div>
           </div>
         </div>
-        <div className='footer-bottom'>
-          <div className='media'>
-            <img src={Twitter} alt='twitter' title="Contact us on Twitter" loading="lazy" />
-            <img src={Instagram} alt='instagram'title="Contact us on instagram" loading="lazy" />
-            <img src={Youtube} alt='youtube'title="Contact us on Youtube" loading="lazy" />
+        <div className="footer-bottom">
+          <div className="media">
+            <img
+              src={Twitter}
+              alt="twitter"
+              title="Contact us on Twitter"
+              loading="lazy"
+            />
+            <img
+              src={Instagram}
+              alt="instagram"
+              title="Contact us on instagram"
+              loading="lazy"
+            />
+            <img
+              src={Youtube}
+              alt="youtube"
+              title="Contact us on Youtube"
+              loading="lazy"
+            />
           </div>
-          <div className='short-links'>
-            <Link to='/terms'>Terms</Link>
-            <Link to='/privacy'>Privacy and policy</Link>
-            <Link to='/payment'>Payment Policy</Link>
-            <Link to='/rules'>Rules</Link>
+          <div className="short-links">
+            <Link to="/terms">Terms</Link>
+            <Link to="/privacy">Privacy and policy</Link>
+            <Link to="/payment">Payment Policy</Link>
+            <Link to="/rules">Rules</Link>
           </div>
-          <div className='payment'>
-            <img src={mastercard} alt='payment' title="payment with MasterCard" loading="lazy" />
-            <img src={Paystack} alt='payment' title="payment with Paystact" loading="lazy"/>
-            <img src={visa} alt='payment'title="payment with visa" loading="lazy" />
-            <img src={age} alt='age' title="18 plus" loading="lazy"/>
+          <div className="payment">
+            <img
+              src={mastercard}
+              alt="payment"
+              title="payment with MasterCard"
+              loading="lazy"
+            />
+            <img
+              src={Paystack}
+              alt="payment"
+              title="payment with Paystact"
+              loading="lazy"
+            />
+            <img
+              src={visa}
+              alt="payment"
+              title="payment with visa"
+              loading="lazy"
+            />
+            <img src={age} alt="age" title="18 plus" loading="lazy" />
           </div>
         </div>
       </div>
